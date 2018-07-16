9001b2a38311e9c771339b32ec8314cc5618ba5e
Arthur Jen <arthurljen@gmail.com>
2018-07-16 15:57:12 -0700

    initial commit

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,16 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = require('chai');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 chai.use(chaiHttp);
 
 describe('Pirates API', () => {
 
-    it('write a test...', () => {
-        assert.isOk(false, 'time to write your first test!');
-        // return chai.request(app)
-        // ...
+    it('gets penguins', () => {
+        return chai.request(app)
+            .get('/api/penguins')
+            .then(({ body }) => {
+                assert.deepEqual(body, ['bernard', 'bernice']);
+            });
     });
 });
