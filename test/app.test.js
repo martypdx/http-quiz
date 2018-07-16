ff73d017fc8c84e6e1a3ee2570d6a31f031af97a
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-16 16:31:19 -0700

    Initial Commit

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,24 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = require('chai');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 chai.use(chaiHttp);
 
-describe('Pirates API', () => {
+describe('API Penguins', () => {
 
     it('write a test...', () => {
         assert.isOk(false, 'time to write your first test!');
         // return chai.request(app)
         // ...
+    });
+    it('responds bernice and bernard on GET', () => {
+        return chai.request(app)
+            .get('/api/Penguins')
+            .set('Content-Type', 'application/json')
+            .then(res => {
+                assert.equal(res.status, 200);
+                assert.equal(res.body, ['bernice', 'bernard]']);    
+            });
+
     });
 });
