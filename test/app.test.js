a0e05a69174c4c6aff19893fe21f26590cfa6a94
MariahAdams <mariah.adams23@yahoo.com>
2018-07-16 16:16:04 -0700

    confirmed first test passing with plain text

--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,16 @@
 const chai = require('chai');
+const { assert } = chai;
 const chaiHttp = require('chai-http');
-const { assert } = require('chai');
-const app = require('../lib/app');
 chai.use(chaiHttp);
+const app = require('../lib/app');
 
 describe('Penguins API', () => {
 
     it('returns penguins on GET', () => {
         return chai.request(app)
-            .get('api/penguins')
+            .get('/api/penguins')
             .then(res => {
-                assert.equal(res.json, ['bernice', 'bernard']);
+                assert.equal(res.text, 'bernard');
             });
     });
 });
