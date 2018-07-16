5f826002c232f682d0a522995a84f2e8a446a5fe
MariahAdams <mariah.adams23@yahoo.com>
2018-07-16 15:57:59 -0700

    initial setup commit

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,16 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = require('chai');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 chai.use(chaiHttp);
 
-describe('Pirates API', () => {
+describe('Penguins API', () => {
 
-    it('write a test...', () => {
-        assert.isOk(false, 'time to write your first test!');
-        // return chai.request(app)
-        // ...
+    it('returns penguins on GET', () => {
+        return chai.request(app)
+            .get('api/penguins')
+            .then(res => {
+                assert.equal(res.json, ['bernice', 'bernard']);
+            });
     });
 });
