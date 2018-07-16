805e99bbb45bf97d205ecdd8802833770ca4b9dc
Mario Quintana <pyrros2000@gmail.com>
2018-07-16 16:37:10 -0700

    Only had one test passing, did spend a lot of time trying to figure out my first test

--- test/app.test.js
+++ test/app.test.js
@@ -1,20 +1,61 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = require('chai');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 chai.use(chaiHttp);
-
-const app = require('../lib/app'); 
-
+ 
 describe('Penguins API', () => {
 
-    it('Returns a json response on GET...', () => {
+    it('Returns an array response on GET...', () => {
         return chai.request(app)
             .get('/api/penguins')
             .then(res => {
                 console.log(res.text);
-                assert.deepEqual(res.text, ['bernice', 'bernard']);
+                assert.equal(res.text, ['bernice', 'bernard']);
                 assert.equal(res.header['content-type'], 'application/json');
+            });
+    });
+
+    it('Returns object on GET with format ', () => {
+        return chai.request(app)
+            .get('/api/penguin/king?')
+            .query('format=<simple|full>')
+            .then(res => {
+                assert.equal(res.text, { 
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
+                assert.equal(res.header['content-type'], 'application/json');
+            });
+    });
+
+    it('Returns object on GET without format ', () => {
+        return chai.request(app)
+            .get('/api/penguin/king?')
+            .then(res => {
+                assert.equal(res.text, { 
+                    name: 'bernice',
+                });
+                assert.equal(res.header['content-type'], 'application/json');
+            });
+    });
+
+    it('DELETE returns html text', () => {
+        return chai.request(app)
+            .get('/mistake')
+            .then(res => {
+                assert.equal(res.text, '<p>All tracks covered</p>');
+                assert.equal(res.header['content-type'], 'text/html');
+            });
+    });
+
+    it('Responds with 404 not invalid path', () => {
+        return chai.request(app)
+            .get('/invalid')
+            .then(res => {
+                assert.equal(res.status, 404);
+                assert.match(res.text, /CANNOT/);
             });
     });
 });
