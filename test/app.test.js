7756581575cfd918016a237826f3ca5f34474f3c
carriecarter <carrie@carrie-carter.com>
2018-07-16 16:26:49 -0700

    pseudo party

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,36 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = require('chai');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 chai.use(chaiHttp);
 
 describe('Pirates API', () => {
 
-    it('returns JSON pirate names', () => {
-        assert.isOk(false, 'time to write your first test!');
+    it('gets pirate names as JSON', () => {
         return chai.request(app)
-       
+            .get('/api/penguins')
+            .then(res => {
+                assert.equal(res.json, ['bernice', 'bernard']);
+            });      
     });
+
+    it('returns penguin JSON object', () => {
+
+    });
+
+    it('returns just the name', () => {
+
+    });
+
+    it('returns tracks covered message on mistake path', () => {
+
+    });
+
+    it('responds with a 404 error', () => {
+
+    });
+
+
+
+
 });
