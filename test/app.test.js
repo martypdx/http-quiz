3663c9e62f1caef9221fd6c2b590d348c509d469
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-16 16:14:16 -0700

    Add query functionality and test

--- test/app.test.js
+++ test/app.test.js
@@ -6,6 +6,12 @@ chai.use(chaiHttp);
 
 describe('Pirates API', () => {
 
+    let bernice = { 
+        name: 'bernice',
+        description: 'What a penguin!',
+        age: 7
+    };
+
     it('responds with hello world on GET', () => {
         return chai.request(app)
             .get('/')
@@ -17,9 +23,17 @@ describe('Pirates API', () => {
 
     it('responds with correct json response', () => {
         return chai.request(app)
-            .get('/api/penguins/')
+            .get('/api/penguins')
             .then(res => {
                 assert.deepEqual(res.body, ['bernice', 'bernard']);
+            });
+    });
+
+    it('responds correctly with queries', () => {
+        return chai.request(app)
+            .get('/api/penguin/king?format=full')
+            .then(res => {
+                assert.deepEqual(res.body, bernice);
             });
     });
 });
